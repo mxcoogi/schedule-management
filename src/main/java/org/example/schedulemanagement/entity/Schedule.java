2c43e435e2e6aef120c1e667fb1e9f0688d6e5ad
mxcoogi <mxcoogi@gmail.com>
2025-03-29 23:53:07 +0900

    feat : Add UserEntity, ScheduleEntity

---
+++ src/main/java/org/example/schedulemanagement/entity/Schedule.java
@@ -0,0 +1,19 @@
+package org.example.schedulemanagement.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "schedule")
+public class Schedule extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+    @Column(columnDefinition = "longtext")
+    private String contents;
+
+
+}
---
+++ src/main/java/org/example/schedulemanagement/entity/User.java
@@ -0,0 +1,21 @@
+package org.example.schedulemanagement.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "user")
+public class User extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+}
